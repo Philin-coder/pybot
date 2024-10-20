1e60c425ce51954f59fe5a2df201058f42e374ef
Alexey <80147483+Philin-coder@users.noreply.github.com>
2024-10-21 00:05:26 +0300

    my first echo_bot

--- bot/echo_bot.py
+++ bot/echo_bot.py
@@ -1,4 +1,13 @@
-from telegram import Update, ForceReply
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-
-pass
+import telebot
+# Создаем экземпляр бота
+bot = telebot.TeleBot('token')
+# Функция, обрабатывающая команду /start
+@bot.message_handler(commands=["start"])
+def start(m, res=False):
+    bot.send_message(m.chat.id, 'Я на связи. Напиши мне что-нибудь )')
+# Получение сообщений от юзера
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    bot.send_message(message.chat.id, 'Вы написали: ' + message.text)
+# Запускаем бота
+bot.polling(none_stop=True, interval=0)
